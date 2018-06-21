d6cdf8e0ec585af7d654e3ed5a6da211653f1356
mehdinosrati <mehdins@aut.ac.ir>
2018-06-21 20:09:22 +0430

    en

---
+++ assets/pass.js
@@ -0,0 +1,8 @@
+function show(){
+	var x = prompt("the old pass u'd have:");
+	if (x === 'sampad1398'){
+		document.getElementById("s").style.display = 'block';
+		document.getElementById("s1").style.display = 'block';
+	}
+}
+show();
